3f25e8ff3403d0b4aa70f7361ff5361478e7af6b
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-09 16:55:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,18 @@
 def prime?(num)
-  divisor = 2
-  if num != num.abs || num == 0 || num == 1
-    return false  
-  elsif num.abs <= 3
-    return true
-  else
-    while divisor <= (num ** 0.5).ceil
-      if num % divisor == 0
-        return false
-      else
-        is_prime = true
+    divisor = 2
+    if num != num.abs || num == 0 || num == 1
+      return false  
+    elsif num.abs <= 3
+      return true
+    else
+      while divisor <= (num ** 0.5).ceil
+        if num % divisor == 0
+          return false
+        else
+          is_prime = true
+        end
+        divisor += 1
       end
-      divisor += 1
     end
+    return is_prime
   end
-  return is_prime
-end
